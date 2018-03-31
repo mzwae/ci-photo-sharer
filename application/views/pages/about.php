2ffa41bc7e54bdedeaf5c06219a116d595075dc5
mzwae <mrmonjaie@gmail.com>
2018-03-31 12:30:39 +0100

    set up project files

---
+++ application/views/pages/about.php
@@ -0,0 +1,2 @@
+<h2><?= $title ?></h2>
+<p>This is Discussion Forum version 1.0. Thank you for using it. I hope you find it useful.</p>
---
+++ application/views/templates/header.php
@@ -0,0 +1,33 @@
+<html>
+  <head>
+    <title>Photo Sharer</title>
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/styles.css">
+  </head>
+  <body>
+
+    <nav class="navbar navbar-inverse">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="container">
+      <div class="navbar-header">
+          <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="<?=base_url()?>">Photo Sharer</a>
+      </div>
+      <!-- Collection of nav links and other content for toggling -->
+      <div id="navbarCollapse" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+              <li class="nav-item"><a href="<?=base_url()?>home">Home</a></li>
+              <li class="nav-item"><a href="<?=base_url()?>about">About</a></li>
+              <li class="nav-item"><a href="<?=base_url()?>discussions/create">Create</a></li>
+
+          </ul>
+      </div>
+    </div>
+</nav>
+
+<div class="container">
